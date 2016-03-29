6ba3d3871aef7328ff454e70fe1e19f77b746433
krezlyreann <kmplata1@up.edu.ph>
2016-03-29 21:08:11 +0800

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,21 +1,12 @@
 package G2E3;
 
-public class Book {
-<<<<<<< HEAD
+public class Book{
 	String id = "";
 	String title = "";
 	String author = "";
 	String year = "";
 
 	public Book( String title, String author, String year) {
-=======
-	protected String id;
-	private String title;
-	private String author;
-	private String year;
-
-	public Book(String title, String author, String year) {
->>>>>>> 7bc9389c41d274bcf3a2e981e87b44da1d360336
 		this.title = title;
 		this.author = author;
 		this.year = year;
